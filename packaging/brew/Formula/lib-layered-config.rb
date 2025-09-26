d3000d13299f080dd2718667778874f668a3aafe
bitranox <rnowotny1966@gmail.com>
2025-09-26 14:26:01 +0200

    chore(packaging): sync for v0.1.0

--- packaging/brew/Formula/lib-layered-config.rb
+++ packaging/brew/Formula/lib-layered-config.rb
@@ -4,19 +4,19 @@ class LibLayeredConfig < Formula
   desc "Layered configuration loader for Python applications"
   homepage "https://github.com/bitranox/lib_layered_config"
   url "https://github.com/bitranox/lib_layered_config/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "<fill-me>"
+yc7d27e880e24eddd5bae6a16fe0e78aa7787b8c85dc571dceec1182354f612"
   license "MIT"
 
   depends_on "python@3.10"
 
   resource "rich-click" do
     url "https://files.pythonhosted.org/packages/29/c2/f08b5e7c1a33af8a115be640aa0796ba01c4732696da6d2254391376b314/rich_click-1.9.1.tar.gz"
-    sha256 "4f2620589d7287f86265432e6a909de4f281de909fe68d8c835fbba49265d268"
+yc7d27e880e24eddd5bae6a16fe0e78aa7787b8c85dc571dceec1182354f612"
   end
 
   resource "lib_cli_exit_tools" do
     url "https://files.pythonhosted.org/packages/c0/0a/263aa633ae8926e9c30f28679be58c84691a9e3423863a053aef07d20338/lib_cli_exit_tools-1.3.1.tar.gz"
-    sha256 "0d7e496a139748e1f31ac01f55d583169ca8c3dded1830426e926499d1ae560c"
+yc7d27e880e24eddd5bae6a16fe0e78aa7787b8c85dc571dceec1182354f612"
   end
 
   resource "tomli" do
